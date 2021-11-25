045ef5feb1f9f3d7493b85270e1adf9a414eafb4
Manuel Marin <manuelmarin1994@gmail.com>
2021-11-25 09:35:13 -0400

    template engine config

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const app = express()
 //Middleware
 app.use(express.json())
 
+// set template engine
+app.set('view engine', 'pug')
+
 //Connection Db
 
 //Routes
-
+app.get('/', function (req, res) {
+  res.render('home');
+});
 //Server Init
 app.listen(port, ()=>{
   console.log(`Server up in http://localhost:${port}`);
