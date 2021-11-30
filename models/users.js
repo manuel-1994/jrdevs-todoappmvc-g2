de625a832a98e05920fc188c4d5f964afcdc54e7
Manuel Marin <manuelmarin1994@gmail.com>
2021-11-30 11:31:16 -0400

    create models folder and model users

---
+++ models/users.js
@@ -0,0 +1,15 @@
+const {mongoose} = require('../config/db')
+
+const {Schema} = mongoose
+
+const userSchema = new Schema({
+  username: String,
+  email: String,
+  password: String,
+  rol: String
+})
+
+//model
+const UserModel = mongoose.model('users', userSchema)
+
+module.exports = UserModel
