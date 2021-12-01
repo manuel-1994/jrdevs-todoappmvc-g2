dcb100c2808c9d18d518640751b84a162e022763
Manuel Marin <manuelmarin1994@gmail.com>
2021-12-01 16:41:17 -0400

    update user router and controller

--- routers/users.js
+++ routers/users.js
@@ -11,10 +11,29 @@ const users = (app) =>{
     res.status(200).json(data)
   })
 
+  router.get('/:email', async (req, res)=>{
+    const email = req.params.email
+    const user = await usersController.getUser(email)
+    res.status(200).json(user)
+  } )
+
   router.post('/', async (req, res) =>{
     const data = req.body
     const newUser = await usersController.createUser(data)
     res.status(201).json(newUser)
+  })
+
+  router.put('/:id', async (req, res) =>{
+    const id = req.params.id
+    const data= req.body
+    const user = await usersController.updateUser(id,data)
+    res.status(200).json(user)
+  })
+
+  router.delete('/:id', async (req, res) =>{
+    const id = req.params.id
+    const user = await usersController.delete(id)
+    res.status(200).json(user)
   })
 }
 
